3cf2f7f5146954eedc248f5cefc82d78d833d33a
maskedwolf4 <meetwadekar7@gmail.com>
2026-02-10 21:33:40 +0530

    question

---
+++ team_analyzer.py
@@ -0,0 +1,3 @@
+"""
+Create a TimeSeriesAnalyzer class that stores timestamp-value pairs and provides methods to calculate moving average and detect trend direction (increasing/decreasing/stable).
+"""
